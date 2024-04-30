e206d986528691b2dbe7c5bc2be2600ed95d3644
tojifushiguroed <116516982+tojifushiguroed@users.noreply.github.com>
2024-04-30 18:28:03 +0300

    Add files via upload

---
+++ java_project/java/com.gtappdevelopers.chatbot/MsgModal.java
@@ -0,0 +1,15 @@
+package com.gtappdevelopers.chatbot;
+
+public classs MsgModal {
+		private String cnt;
+
+		public String getCnt() {
+			return cnt;
+		}
+		public void setCnt(string cnt) {
+			this.cnt = cnt;
+		}
+		public MsgModal(String cnt) {
+			this.cnt = cnt;
+		}
+}
